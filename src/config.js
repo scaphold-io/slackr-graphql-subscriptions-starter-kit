b3cb8db464840d7f0251ce1c6677cbc038099f6d
Michael Paris <michael@scaphold.io>
2017-03-25 14:48:59 -0700

    Update for new apollo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  scapholdUrl: 'localhost:3000/graphql/jobless-care',
+  scapholdUrl: 'us-west-2.api.scaphold.io/graphql/slackr-api',
   auth0ClientId: 'AyT62DFIEaJx7HoflGOWbzduvIyFm76K',
   auth0Domain: 'slackr.auth0.com',
 }
